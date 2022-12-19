b54a24ce94d60f62a603f59b73f497d085de7e70
wantJoy1 <wantjoy1@gmail.com>
2022-12-20 07:15:31 +0900

    add guessing function

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 use ferris_says::say;
-use std::io::{stdout, BufWriter};
+use std::io::{stdout, BufWriter, stdin};
 
 fn main() {
     println!("Hello, world!");
     ferris_say(String::from("Hello fellow Rustaceans!"));
+    guessing();
 }
 
 fn ferris_say(message: String) {
@@ -12,4 +13,17 @@ fn ferris_say(message: String) {
 
     let mut writer = BufWriter::new(stdout.lock());
     say(message.as_bytes(), width, &mut writer).unwrap();
+}
+
+fn guessing() {
+    println!("Guess the number!");
+    println!("Please input your guess.");
+    let mut guess = String::new();
+
+    stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+
+    println!("You guessed: {}", guess);
 }

02120d75fec371ac2bf59c3024accb9b30b116e1
wantJoy1 <wantjoy1@gmail.com>
2022-12-20 09:32:11 +0900

    compare numbers

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use ferris_says::say;
 use std::io::{stdout, BufWriter, stdin};
+use std::cmp::Ordering;
+use rand::Rng;
 
 fn main() {
     println!("Hello, world!");
@@ -17,13 +19,23 @@ fn ferris_say(message: String) {
 
 fn guessing() {
     println!("Guess the number!");
+    let secret_number = rand::thread_rng().gen_range(1..101);
+    println!("The secret number is: {}", secret_number);
     println!("Please input your guess.");
     let mut guess = String::new();
 
     stdin()
         .read_line(&mut guess)
         .expect("Failed to read line");
-
+    
+    let guess: u32 = guess.trim().parse()
+        .expect("Please type a number!");
 
     println!("You guessed: {}", guess);
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
 }

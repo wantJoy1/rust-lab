95366cfbd8168cfaa9a551d547bb3057985dd8b8
wantJoy1 <wantjoy1@gmail.com>
2022-12-23 08:51:14 +0900

    add twitter example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+mod twitter;
+
 use ferris_says::say;
 use std::io::{stdout, BufWriter, stdin};
 use std::cmp::Ordering;
@@ -63,4 +65,33 @@ fn get_str() -> String {
         .read_line(&mut guess)
         .expect("Failed to read line");
     guess
+}
+
+fn tweets () {
+    use twitter_v2::TwitterApi;
+    use twitter_v2::authorization::{Oauth2Token, BearerToken};
+    use twitter_v2::query::{TweetField, UserField};
+
+    let auth = BearerToken::new(std::env::var("APP_BEARER_TOKEN").unwrap());
+    let tweet = TwitterApi::new(auth)
+        .get_tweet(1261326399320715264)
+        .tweet_fields([TweetField::AuthorId, TweetField::CreatedAt])
+        .send()
+        .await?
+        .into_data()
+        .expect("this tweet should exist");
+    assert_eq!(tweet.id, 1261326399320715264);
+    assert_eq!(tweet.author_id.unwrap(), 2244994945);
+    assert_eq!(tweet.created_at.unwrap(), datetime!(2020-05-15 16:03:42 UTC));
+
+    let auth: Oauth2Token = serde_json::from_str(&stored_oauth2_token)?;
+    let my_followers = TwitterApi::new(auth)
+        .with_user_ctx()
+        .await?
+        .get_my_followers()
+        .user_fields([UserField::Username])
+        .max_results(20)
+        .send()
+        .await?
+        .into_data();
 }
